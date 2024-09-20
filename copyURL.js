c342a249d141b25b9b4dbd8270469b23dec758bc
Liz Kenyon <lizkenyon07@gmail.com>
2024-09-20 15:51:43 -0500

    Update copyURL.js

--- copyURL.js
+++ copyURL.js
@@ -2,7 +2,7 @@ async function copyURL() {
 const url = window.location.href;
 const trimedURL = url.replace('https://shopify.dev', '');
 const x = document.createElement('input'); 
-x.value = url; 
+x.value = trimmedURL; 
 document.body.appendChild(x); 
 x.select(); document.execCommand('copy'); 
 document.body.removeChild(x);
